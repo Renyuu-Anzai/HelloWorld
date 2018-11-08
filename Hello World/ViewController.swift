e0a23b4f46f305a7bfb731711d45e0535ad10370
RENYU ANZAI <r.anzai@slj.jp>
2018-11-08 23:34:26 +0900

    Initial Commit

---
+++ Hello World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by 安西蓮有 on 2018/11/08.
+//  Copyright © 2018年 Renyuu.Anzai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
